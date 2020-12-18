9ed5a11bfce6677b1f0bfee306778709faffca12
ram-asokan93 <ramkumar.asokan@my.jcu.edu.au>
2020-12-18 19:25:25 +0800

    Refactor score evaluator to use function and use random integer for calculation

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,15 +2,23 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-# Fixed !
 
-score = float(input("Enter score: "))
-if 100 > score < 0:
-    print("Invalid score")
-elif score >= 90:
-    print("excellent")
-elif score >= 50:
-    print("passable")
-else:
-    print("bad")
+def main():
+    score = random.randint(0, 100)
+    print(evaluate_score(score))
+
+
+def evaluate_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
