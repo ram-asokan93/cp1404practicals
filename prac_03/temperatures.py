636369513f6bcd235ca94ed2e57749f433db2644
ram-asokan93 <ramkumar.asokan@my.jcu.edu.au>
2020-12-18 19:24:19 +0800

    Refactor temperature calculator to use functions

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -8,14 +8,26 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def celsius_to_fahrenheit():
+    global fahrenheit
+    fahrenheit = celsius * 9.0 / 5 + 32
+
+
+def fahrenheit_to_celsius():
+    global celsius
+    celsius = 5 / 9.0 * (fahrenheit - 32)
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        celsius_to_fahrenheit()
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9.0 * (fahrenheit - 32)
+        fahrenheit_to_celsius()
         print("Result: {:.2f} C".format(celsius))
         # Hint: celsius = 5 / 9 * (fahrenheit - 32)
         # Remove the "pass" statement when you are done. It's a placeholder.
